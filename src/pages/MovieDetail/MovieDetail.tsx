41e6432123f3058b0bde8179d538f51979502ad7
Lucas Lencina <lucasfacdef@gmail.com>
2026-09-05 13:03:41 -0300

    agrega detalle de peliculas

--- src/pages/MovieDetail/MovieDetail.tsx
+++ src/pages/MovieDetail/MovieDetail.tsx
@@ -1,5 +1,31 @@
+import { useParams } from "react-router";
+import { useMovies } from "../../hooks/useMovies";
+
 function MovieDetail() {
-  return <h1>STREAMTUC - DETALLE DE PELÍCULA</h1>;
+  const { id } = useParams();
+  const { movies } = useMovies();
+
+  const movie = movies.find((movie) => movie.id === id);
+
+  if (!movie) {
+    return <h1>Película no encontrada</h1>;
+  }
+
+  return (
+    <main>
+      <h1>{movie.title}</h1>
+
+      <img src={movie.image} alt={movie.title} />
+
+      <p>{movie.description}</p>
+
+      <p>Año: {movie.year}</p>
+
+      <p>Categoría: {movie.category}</p>
+
+      <p>Calificación: {movie.rating}</p>
+    </main>
+  );
 }
 
 export default MovieDetail;
