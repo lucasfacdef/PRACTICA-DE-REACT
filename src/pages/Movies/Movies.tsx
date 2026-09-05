5b2f48d6b6232ecbc9802c8471fc4cba6efd5010
Lucas Lencina <lucasfacdef@gmail.com>
2026-09-05 12:58:44 -0300

    agrega pagina de series

--- src/pages/Movies/Movies.tsx
+++ src/pages/Movies/Movies.tsx
@@ -1,5 +1,29 @@
+import MovieCard from "../../components/MovieCard/MovieCard";
+import { useMovies } from "../../hooks/useMovies";
+
 function Movies() {
-  return <h1>STREAMTUC - PELÍCULAS</h1>;
+  const { movies } = useMovies();
+
+  const movieList = movies.filter((movie) => movie.type === "movie");
+
+  return (
+    <main className="movies-page">
+      <h1>Películas</h1>
+
+      <div className="movies-grid">
+        {movieList.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            image={movie.image}
+            year={movie.year}
+            rating={movie.rating}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default Movies;
--- src/pages/Series/Series.tsx
+++ src/pages/Series/Series.tsx
@@ -1,5 +1,29 @@
+import MovieCard from "../../components/MovieCard/MovieCard";
+import { useMovies } from "../../hooks/useMovies";
+
 function Series() {
-  return <h1>STREAMTUC - SERIES</h1>;
+  const { movies } = useMovies();
+
+  const seriesList = movies.filter((movie) => movie.type === "series");
+
+  return (
+    <main className="movies-page">
+      <h1>Series</h1>
+
+      <div className="movies-grid">
+        {seriesList.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            image={movie.image}
+            year={movie.year}
+            rating={movie.rating}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default Series;
